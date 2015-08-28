7ed6b0855621519065cdff7a6e8ba57a58590ad9
liuxijin <liuxijin@meituan.com>
2015-08-28 14:25:55 +0800

    init

---
+++ index.js
@@ -0,0 +1,38 @@
+function createScript(url) {
+    var script = document.createElement('script');
+    script.src = url;
+    return script;
+}
+
+function createCss(url) {
+    var link = document.createElement('link');
+    link.rel = "stylesheet";
+    link.type = "text/css";
+    link.href = url;
+    return link;
+}
+
+function createTask(url) {
+    var item;
+    if (/js$/.test(url)) item = createScript(url);
+    if (/css$/.test(url)) item = createCss(url);
+
+    return new Promise(function(resolve) {
+        var item = processor(x);
+        document.head.appendChild(link);
+        item.onload = resolve;
+        item.onerror = resolve;
+    });
+}
+
+module.exports = function() {
+    var urls = [].slice.call(args);
+    return new Promise(function(resolve) {
+        function next() {
+            if (!urls.length) resolve();
+            var url = urls.shift();
+            createTask(url).then(next);
+        }
+        next();
+    });
+};
